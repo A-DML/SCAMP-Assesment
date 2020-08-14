a2ef721e20154f9969455274ce02841715a2e816
Damola <62553246+A-DML@users.noreply.github.com>
2020-08-14 08:58:34 +0100

    Add files via upload
    Code entry for the SCA mentorship program.

---
+++ script.js
@@ -0,0 +1,19 @@
+// The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two.
+
+
+var fibonacci_series = function (n) 
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+ console.log(fibonacci_series(8));
+ 
